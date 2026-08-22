18e991f31945699ea4b4d25be7bc9d51535c9848
jaymore-sys <jay.more@valencianutrition.com>
2026-08-22 16:42:07 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "blanchedalmond-turkey-793003.hostingersite.com",
 });
 
 api.interceptors.request.use(
